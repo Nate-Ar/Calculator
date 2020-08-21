edf8fb38fbb725afa6944b2e47e1cfec78ed63e2
Nate <45805730+Nate-Ar@users.noreply.github.com>
2020-08-21 00:13:25 -0500

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,25 @@
+
+while True:
+    aos = input('Do you want to add, subtract, multiply, division or quit() $ ')
+    # Add
+    if aos.lower() == 'add':
+        numb1 = input('First Number$ ')
+        numb2 = input('Second Number$ ')
+        print(int(numb1) + int(numb2))
+    # Subtraction
+    if aos.lower() == 'subtract':
+        numb1 = input('First Number$ ')
+        numb2 = input('Second Number$ ')
+        print(int(numb1) - int(numb2))
+    # Multiply
+    if aos.lower() == 'multiply':
+        numb1 = input('First Number$ ')
+        numb2 = input('Second Number$ ')
+        print(int(numb1) * int(numb2))
+    # Division
+    if aos.lower() == 'division':
+        numb1 = input('First Number$ ')
+        numb2 = input('Second Number$ ')
+        print(int(numb1) / int(numb2))
+    if aos.lower() == 'quit()':
+        quit()
