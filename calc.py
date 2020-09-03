389b7edcd273e9f16285d527fbdaad48c904c854
Nate <45805730+Nate-Ar@users.noreply.github.com>
2020-09-02 20:25:08 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
-
+# Creator: Nathan A
+# Description: Does your vocab for you
+# Creation Date: 8/3/2020
+# Update Date: 
 while True:
     aos = input('Do you want to add, subtract, multiply, division or quit() $ ')
     # Add
